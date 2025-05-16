c046ca1e07b2432aea88c728e66e327cb231d1de
David Lopez <davidlopezing.1@gmail.com>
2025-05-16 08:58:54 -0500

    Actualizado

---
+++ app/src/main/java/com/salomon/appmvvm/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,45 @@
+package com.salomon.appmvvm.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salomon.appmvvm.data.model.UserModel
+import com.salomon.appmvvm.data.model.response.LoginResponse
+import com.salomon.appmvvm.domain.PostLoginRequest
+import kotlinx.coroutines.launch
+
+class LoginViewModel: ViewModel() {
+
+    val _loginResponse = MutableLiveData<LoginResponse>()
+    val _userModel = MutableLiveData<UserModel?>()
+
+    // Feedback de carga
+    val _isLoading = MutableLiveData<Boolean>()
+    val _message = MutableLiveData<String>()
+
+    val loginUseCase = PostLoginUseCase()
+
+    fun login(loginRequest: PostLoginRequest){
+
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+
+            val result = loginUseCase(loginRequest)
+            _loginResponse.postValue(result)
+
+            when(result.status){
+                "success" ->{
+                    _userModel.postValue(result.data)
+                }
+                "invalid" ->{
+                    _message.postValue(result.message)
+                    _isLoading.postValue(false)
+                }
+                "error" ->{
+                    _message.postValue("Usuario no registrado! \uD83D\uDE14")
+                    _isLoading.postValue(false)
+                }
+            }
+        }
+    }
+}
